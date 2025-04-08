214c6bde334092ebfa6b5c837317f7688166c893
Thane Chaves <tmoonder@gmail.com>
2025-04-08 15:24:31 -0400

    Connected search component to API (without authorization).

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -2,9 +2,29 @@ import { useState } from 'react'
 
 export function Search() {
   const [results, setResults] = useState([])
-  const handleChange = () => {
-    setResults(['example'])
+
+  async function getResults(input) {
+    try {
+      await fetch(`https://api.spotify.com/v1/search${input}`)
+        .then((res) => {
+          if (res.status >= 400) {
+            throw res.status
+          }
+          return res.json()
+        })
+        .then((json) => {
+          setResults(json)
+        })
+    } catch (error) {
+      throw new Error('Could not connect to API.')
+    }
   }
+
+  const handleChange = (event) => {
+    let input = event.target.value
+    getResults(input)
+  }
+
   return (
     <>
       <input
