d6bed156ea0cce1ad7a16e151cd0f839815652bb
Thane Chaves <tmoonder@gmail.com>
2025-04-08 15:06:19 -0400

    Added basic autocomplete template to search component.

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,10 +1,24 @@
+import { useState } from 'react'
+
 export function Search() {
+  const [results, setResults] = useState([])
+  const handleChange = () => {
+    setResults(['example'])
+  }
   return (
-    <input
-      type="search"
-      name="search"
-      placeholder="Search"
-      aria-label="Search"
-    />
+    <>
+      <input
+        type="search"
+        name="search"
+        placeholder="Search"
+        aria-label="Search"
+        onChange={handleChange}
+      />
+      <ul>
+        {results.map((item) => (
+          <li>{item}</li>
+        ))}
+      </ul>
+    </>
   )
 }
