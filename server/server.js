fd8802678878eb4709bfc4e777b6d0fcac83e54a
Thane Chaves <tmoonder@gmail.com>
2025-04-14 18:41:03 -0400

    Added .env file, dotenv and pkg dependencies, and server pool.

--- server/server.js
+++ server/server.js
@@ -1,7 +1,17 @@
 import express from 'express'
 import cors from 'cors'
+import pkg from 'pg'
+import 'dotenv/config'
+
 const APP = express()
 const PORT = 5000
+const { Pool } = pkg
+const pool = new Pool({
+  user: process.env.USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD
+})
 
 APP.use(cors())
 APP.use(express())
