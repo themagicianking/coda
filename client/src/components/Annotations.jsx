8f9b4af3aabca0819136175d1671533da8d55470
Thane Chaves <tmoonder@gmail.com>
2025-04-14 16:07:52 -0400

    Created an annotations component with necessary states and functions that are required to fetch information from the server.

---
+++ client/src/components/Annotations.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react'
+export default function Annotations({ orderNum }) {
+  const [song, setSong] = useState({})
+  const [hasPrevSong, setHasPrevSong] = useState(false)
+  const [hasNextSong, setHasNextSong] = useState(false)
+  const prevOrderNum = orderNum - 1
+  const nextOrderNum = orderNum + 1
+
+  const sampleSong = {
+    songID: 0,
+    spotifyID: 19093,
+    order: 0,
+    artist: 'Indigo Girls',
+    title: 'Closer to Fine'
+  }
+
+  if (prevOrderNum >= 0) {
+    setHasPrevSong(true)
+  }
+
+  async function getSong() {
+    try {
+      // fetch song from server
+      console.log('This is when the song will be fetched from the server.')
+      console.log(`It should be #${orderNum} in order.`)
+      setSong(sampleSong)
+    } catch (error) {
+      throw new Error(
+        `Could not fetch song data from server. The following error occurred: ${error}`
+      )
+    }
+  }
+
+  async function updateNext() {
+    try {
+      console.log(
+        `Checking to see if a song with order ${nextOrderNum} exists.`
+      )
+      console.log('If the get request returns true, set has next to true.')
+      setHasNextSong(true)
+    } catch (error) {
+      throw new Error(
+        `Could not check if next song exists. The following error occurred: ${error}`
+      )
+    }
+  }
+
+  getSong()
+  updateNext()
+}
