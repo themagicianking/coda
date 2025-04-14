f21753eda354db6947e2288b3d82706f8bc63a15
Thane Chaves <tmoonder@gmail.com>
2025-04-14 14:43:44 -0400

    Added comment to explain crypto.randomUUID.

--- client/src/components/Selection.jsx
+++ client/src/components/Selection.jsx
@@ -4,7 +4,10 @@ import SongList from './SongList.jsx'
 export function Selection() {
   const [selected, setSelected] = useState([])
   const addToSelected = (song) => {
-    setSelected([...selected, {...song, songID: crypto.randomUUID() }])
+    // crypto.randomUUID generates a unique index to use as a key.
+    // this is necessary if the same song is selected multiple times:
+    // the id given to it from spotify is no longer unique.
+    setSelected([...selected, { ...song, songID: crypto.randomUUID() }])
   }
   const handleRemove = (songID) => {
     let newList = selected.filter((song) => songID != song['songID'])
