88ac939abdfc0ccd6fa87bba564a0944f18e26d9
Thane Chaves <tmoonder@gmail.com>
2025-04-16 16:36:06 -0400

    Addedd basic route setup with react router dom.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
-import {Annotations} from "./components/Annotations"
-// import { Selection } from './components/Selection'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Annotations } from './components/Annotations'
+import { Selection } from './components/Selection'
 function App() {
-  // return <Selection />
-  return <Annotations />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/selection" element={<Selection />} />
+        <Route path="/annotations" element={<Annotations />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
