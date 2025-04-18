b05389688928cff24eb0482210482099768af5ac
Thane Chaves <tmoonder@gmail.com>
2025-04-18 19:51:43 -0400

    Created component for playlist viewing page.

---
+++ client/src/components/Playlist.jsx
@@ -0,0 +1,14 @@
+export function Playlist() {
+  return (
+    <div>
+      <h1>Playlist Name</h1>
+      <h2>From Name to Name</h2>
+      <p>General description of playlist</p>
+      <ol>
+        <li>Song one will go here</li>
+        <li>Song two will go here</li>
+        <li>Song three will go here</li>
+      </ol>
+    </div>
+  )
+}
