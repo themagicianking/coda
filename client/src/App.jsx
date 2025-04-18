25101eeb1b1bb998459bc43d332462e351937747
Thane Chaves <tmoonder@gmail.com>
2025-04-18 19:53:58 -0400

    Added route for playlist to app component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Annotations } from './components/Annotations'
+import { Playlist } from './components/Playlist'
 import { Selection } from './components/Selection'
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path="/selection" element={<Selection />} />
         <Route path="/annotations" element={<Annotations />} />
+        <Route path="/playlist" element={<Playlist />} />
       </Routes>
     </Router>
   )
