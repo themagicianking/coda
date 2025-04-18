6ba2d145049763a524f552e838a92e158d284262
Thane Chaves <tmoonder@gmail.com>
2025-04-18 14:22:43 -0400

    Added getAllSongs method to selection.

--- client/src/components/Selection.jsx
+++ client/src/components/Selection.jsx
@@ -1,13 +1,36 @@
-import { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import Search from './Search.jsx'
 import SongList from './SongList.jsx'
 export function Selection() {
   const [selected, setSelected] = useState([])
+  const navigate = useNavigate()
+
+  async function getAllSongs() {
+    try {
+      await fetch('http://localhost:5000/allsongs')
+        .then((res) => {
+          if (res.status >= 400) {
+            throw res.status
+          }
+          return res.json()
+        })
+        .then((json) => {
+          setSelected(json)
+          console.log(json)
+        })
+    } catch (error) {
+      throw new Error(
+        `Could not get songs from server. The following error occurred: ${error}`
+      )
+    }
+  }
+  useEffect(() => {
+    getAllSongs()
+  }, [])
+
   const addSong = (song) => {
-    setSelected([
-      ...selected,
-      { ...song, songorder: selected.length }
-    ])
+    setSelected([...selected, { ...song, songorder: selected.length }])
   }
   const removeSong = (songorder) => {
     let newList = selected.filter((song) => songorder != song.songorder)
