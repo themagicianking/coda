b6a3c251acbea8065352641b042a7039f0a968ef
Thane Chaves <tmoonder@gmail.com>
2025-04-19 02:15:46 -0400

    Adjusted cassette background image positioning.

--- client/src/components/Cassette.jsx
+++ client/src/components/Cassette.jsx
@@ -2,7 +2,7 @@ export function Cassette() {
   return (
     <article className="cassette">
       <div className="cassetteImage">
-        <h2>Click here to make your playlist</h2>
+        <h1>Click here to make your playlist</h1>
       </div>
     </article>
   )
