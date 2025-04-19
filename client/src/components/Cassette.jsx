4c3fd46b4e7305d6fb24a89d79478b0f129172b1
Thane Chaves <tmoonder@gmail.com>
2025-04-19 00:42:40 -0400

    Switched to skeleton css and created a file for welcome page css.

--- client/src/components/Cassette.jsx
+++ client/src/components/Cassette.jsx
@@ -1,6 +1,6 @@
 export function Cassette() {
   return (
-    <article class="yellow rounded padding">
+    <article id="cassette"className="yellow rounded padding">
       <h2>Click here to make your playlist</h2>
     </article>
   )
--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,7 @@
 import { AboutApp } from './AboutApp'
 import { Cassette } from './Cassette'
 import { Credits } from './Credits'
+import './welcome.css'
 
 export function Welcome() {
   return (
