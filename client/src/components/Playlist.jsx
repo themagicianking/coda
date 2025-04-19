40b7647b03586eeb4ca910e099089997323c6ae8
Thane Chaves <tmoonder@gmail.com>
2025-04-18 20:01:59 -0400

    Connected playlist component to database.

--- client/src/components/Playlist.jsx
+++ client/src/components/Playlist.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from 'react'
+
 export function Playlist() {
+  const [songs, setSongs] = useState([])
+
+  async function getAllSongs() {
+    try {
+      await fetch('http://localhost:5000/allsongs')
+        .then((res) => {
+          if (res.status >= 400) {
+            throw res.status
+          }
+          console.log('Got all songs from the server.')
+          return res.json()
+        })
+        .then((json) => {
+          setSongs(json)
+        })
+    } catch (error) {
+      throw new Error(
+        `Could not get songs from server. The following error occurred: ${error}`
+      )
+    }
+  }
+
+  useEffect(() => {
+    getAllSongs()
+  }, [])
+
   return (
     <div>
       <h1>Playlist Name</h1>
       <h2>From Name to Name</h2>
       <p>General description of playlist</p>
       <ol>
-        <li>Song one will go here</li>
-        <li>Song two will go here</li>
-        <li>Song three will go here</li>
+        {songs.map((song) => (
+          <li>{JSON.stringify(song)}</li>
+        ))}
       </ol>
     </div>
   )
