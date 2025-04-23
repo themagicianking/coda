5c81407132570a14fb89b3380303e33706f9c743
Thane Chaves <tmoonder@gmail.com>
2025-04-23 12:09:26 -0400

    Added function to get access token to the client.

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -1,39 +1,59 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Result from './Result.jsx'
 
 export default function Search({ handleSelect }) {
   const [results, setResults] = useState([])
-  const sampleItems = [
-    {
-      spotifyid: 0,
-      artist: 'Indigo Girls',
-      title: 'Closer to Fine',
-      lyrics: '',
-      note: ''
-    },
-    {
-      spotifyid: 1,
-      artist: 'CHVRCHES',
-      title: 'Empty Threat',
-      lyrics: '',
-      note: ''
-    },
-    {
-      spotifyid: 2,
-      artist: 'Twenty One Pilots',
-      title: 'Stressed Out',
-      lyrics: '',
-      note: ''
-    },
-    { spotifyid: 3, artist: 'AJR', title: 'Bang', lyrics: '', note: '' },
-    {
-      spotifyid: 4,
-      artist: 'cavetown',
-      title: 'This is Home',
-      lyrics: '',
-      note: ''
+  const [accessToken, setAccessToken] = useState()
+  // const sampleItems = [
+  //   {
+  //     spotifyid: 0,
+  //     artist: 'Indigo Girls',
+  //     title: 'Closer to Fine',
+  //     lyrics: '',
+  //     note: ''
+  //   },
+  //   {
+  //     spotifyid: 1,
+  //     artist: 'CHVRCHES',
+  //     title: 'Empty Threat',
+  //     lyrics: '',
+  //     note: ''
+  //   },
+  //   {
+  //     spotifyid: 2,
+  //     artist: 'Twenty One Pilots',
+  //     title: 'Stressed Out',
+  //     lyrics: '',
+  //     note: ''
+  //   },
+  //   { spotifyid: 3, artist: 'AJR', title: 'Bang', lyrics: '', note: '' },
+  //   {
+  //     spotifyid: 4,
+  //     artist: 'cavetown',
+  //     title: 'This is Home',
+  //     lyrics: '',
+  //     note: ''
+  //   }
+  // ]
+
+  async function getCredentials() {
+    try {
+      await fetch('http://localhost:5000/credentials')
+        .then((res) => {
+          if (res.status >= 400) {
+            throw res.status
+          }
+          return res.json()
+        })
+        .then((json) => {
+          setAccessToken(json.accesstoken)
+        })
+    } catch (error) {
+      throw new Error(
+        `Could not get credentials from server. The following error occurred: ${error}`
+      )
     }
-  ]
+  }
 
   // async function getResults(input) {
   //   try {
@@ -53,6 +73,10 @@ export default function Search({ handleSelect }) {
   //     )
   //   }
   // }
+
+  useEffect(() => {
+    getCredentials()
+  }, [])
 
   const handleSearch = () => {
     // let input = event.target.value
