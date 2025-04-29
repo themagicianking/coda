0d0c5b96ea25d87bbdc6f67debb061949967704a
Thane Chaves <tmoonder@gmail.com>
2025-04-28 20:20:57 -0400

    Corrected urls in annotation component.

--- client/src/components/Annotations.jsx
+++ client/src/components/Annotations.jsx
@@ -2,7 +2,10 @@ import { useNavigate } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { NoteInput } from './NoteInput'
 import { SongInfo } from './SongInfo'
+import { useContext } from 'react'
+import { ServerContext } from './ServerContext'
 export function Annotations() {
+  const SERVER_URL = useContext(ServerContext)
   const [orderNum, setOrderNum] = useState(0)
   const [song, setSong] = useState()
   const [hasPrevSong, setHasPrevSong] = useState(false)
@@ -25,7 +28,7 @@ export function Annotations() {
 
   async function getNextSong() {
     try {
-      await fetch(`http://localhost:5000/nextsong?songorder=${orderNum}`)
+      await fetch(`${SERVER_URL}/nextsong?songorder=${orderNum}`)
         .then((res) => {
           if (res.status >= 400) {
             throw res.status
@@ -53,7 +56,7 @@ export function Annotations() {
 
   async function getPrevSong() {
     try {
-      await fetch(`http://localhost:5000/prevsong?songorder=${orderNum}`)
+      await fetch(`${SERVER_URL}/prevsong?songorder=${orderNum}`)
         .then((res) => {
           if (res.status >= 400) {
             throw res.status
@@ -82,7 +85,7 @@ export function Annotations() {
 
   async function putNote() {
     try {
-      await fetch('http://localhost:5000/note', {
+      await fetch(`${SERVER_URL}/note`, {
         method: 'PUT',
         body: JSON.stringify({ note: song.note, songorder: song.songorder }),
         headers: { 'Content-Type': 'application/json' }
@@ -104,7 +107,7 @@ export function Annotations() {
   async function updateNext(nextOrderNum) {
     try {
       await fetch(
-        `http://localhost:5000/nextsongexists?songorder=${nextOrderNum}`
+        `${SERVER_URL}/nextsongexists?songorder=${nextOrderNum}`
       )
         .then((res) => {
           if (res.status >= 400) {
@@ -130,7 +133,7 @@ export function Annotations() {
   async function updatePrev(prevOrderNum) {
     try {
       await fetch(
-        `http://localhost:5000/prevsongexists?songorder=${prevOrderNum}`
+        `${SERVER_URL}/prevsongexists?songorder=${prevOrderNum}`
       )
         .then((res) => {
           if (res.status >= 400) {
