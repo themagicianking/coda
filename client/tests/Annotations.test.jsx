af390a56f1f9aee128f5066af987d50e9e955be4
Thane Chaves <tmoonder@gmail.com>
2025-04-29 18:47:57 -0400

    Added to do list to annotations test for after context provider is added.

--- client/tests/Annotations.test.jsx
+++ client/tests/Annotations.test.jsx
@@ -3,6 +3,11 @@ import { describe, it } from 'vitest'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Annotations } from '/src/components/Annotations'
 
+// todo: write mock API calls once merged in w branch that uses context provider
+// todo: write test that checks that songInfo and noteInput are rendered if song exists and error message is rendered if song does not exist
+// todo: write test that checks that previous song button is rendered if hasPrevSong is true and that it is not rendered if hasPrevSong is false
+// todo: write test that checks that next song button is rendered if hasNextSong is true and that it is not rendered if hasNextSong is false
+
 describe('Annotations', () => {
   it('renders the Annotation component', () => {
     render(
@@ -13,4 +18,5 @@ describe('Annotations', () => {
 
     screen.debug()
   })
+  it('renders SongInfo and NoteInput', () => {})
 })
